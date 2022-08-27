bb8aff3f1056ac08ba321ed9a459ad2eba8fdb3b
Josia Orava <josia.orava@gmail.com>
2022-08-27 14:30:15 +0300

    exercise2 ready

--- exercise2/main.c
+++ exercise2/main.c
@@ -4,18 +4,20 @@ int main(){
     float salaries[12];
     int x = 0;
     float sum = 0;
-    char equal = "=";
     while(x < 12){
-        printf("Give month %d salarie: ", x);
+        printf("Give month %d salarie: ", x+1);
         fflush(stdin);
-        scanf("%.2f", &salaries[x]);
+        scanf("%f", &salaries[x]);
         x++;
     }
     for (int i = 0; i < 12; i++){
-        printf("month[ %d] %3.c  %10.2f\n", i, equal, salaries[i]);
+        if (i < 9){
+            printf("month[ %d] %*s  %10.2f\n", i+1, 2, "=", salaries[i]);
+        } else{
+            printf("month[%d] %*s  %10.2f\n", i+1, 2, "=", salaries[i]);
+        }
         sum = sum + salaries[i];
     }
-    printf("%.2f", sum);
-
+    printf("total %*s %11.2f",6,"=", sum);
     return 0;
 }
